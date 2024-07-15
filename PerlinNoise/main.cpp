1792ba2258b53ab6e9ef0b18097b2e69f7856e6c
ShavelDev <nor.szaw@gmail.com>
2024-07-15 19:33:11 +0200

    Initial Commit

---
+++ PerlinNoise/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PerlinNoise
+//
+//  Created by Norbert Szawloga on 15/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
