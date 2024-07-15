59efff58f1c0deac2aef867a11a87ba2295864c1
ShavelDev <nor.szaw@gmail.com>
2024-07-15 23:58:45 +0200

    fixed the function, constant parameters

--- PerlinNoise/main.cpp
+++ PerlinNoise/main.cpp
@@ -15,6 +15,10 @@ Point grads[4] = {
     {1,1}, {-1,1} , {-1,-1}, {1,-1}
 };
 
+//Point grads[4] = {
+//    {1.4,0}, {0,1.4} , {-1.4,0}, {0,-1.4}
+//};
+
 float fadeFunc(float x){
     
     return 6*pow(x, 5) - 15*pow(x, 4) + 10*pow(x, 3);
@@ -34,11 +38,11 @@ std::vector<std::vector<float>> generatePerlinNoise(int width, int height, int p
     
     //increasing dimensions of vector in to accomodate the the excessive pixels
     if (width % pixelsPerCell != 0) {
-        width += (width % pixelsPerCell);
+        width += pixelsPerCell - (width % pixelsPerCell);
     }
     
     if (height % pixelsPerCell != 0) {
-        height += (height % pixelsPerCell);
+        height += pixelsPerCell - (height % pixelsPerCell);
     }
     std::vector<std::vector<float>> noise(width, std::vector<float>(height, 0.0f));
 
@@ -53,6 +57,7 @@ std::vector<std::vector<float>> generatePerlinNoise(int width, int height, int p
     float stepPerPixel =  (1 / (float)pixelsPerCell);
     int gridWidth = width / pixelsPerCell;
     int gridHeight = height / pixelsPerCell;
+    
     std::vector<std::vector<Point>> gradients;
     
     gradients.resize(gridWidth + 1);
@@ -64,12 +69,16 @@ std::vector<std::vector<float>> generatePerlinNoise(int width, int height, int p
 
 
     for (int x = 0; x < gradients.size(); x++) {
-        for (int y =0; y < gradients[x].size(); y++) {
-            int rNum = std::rand() % 4;
+        for (int y = 0; y < gradients[x].size(); y++) {
+            int rNum = rand() % 4;
+            cout << rNum << endl;
             gradients[x][y] = grads[rNum];
+            cout << gradients[x][y].x << " " << gradients[x][y].y << endl;
         }
     }
     
+    cout << width << " " << height << endl;
+    cout << gridWidth << " " << gridHeight << endl;
     
 //    int xOffset = 0, yOffset = 0;
 //    int currPixel = 0;
@@ -89,9 +98,22 @@ std::vector<std::vector<float>> generatePerlinNoise(int width, int height, int p
     for (int x = 0; x < noise.size(); x++) {
         for (int y = 0; y < noise[x].size(); y++) {
             
-            Point currPixel = {(float)((x % pixelsPerCell)*stepPerPixel), (float)((y % pixelsPerCell)*stepPerPixel)};
+            
+            
+            //Point currPixel = {(float)(x % pixelsPerCell), (float)(y % pixelsPerCell)};
+            float xRel = x % pixelsPerCell;
+            float yRel = y % pixelsPerCell;
+            
+            Point v00 = {xRel*stepPerPixel, yRel*stepPerPixel};
+            Point v10 = {-(1-xRel*stepPerPixel), yRel*stepPerPixel};
+            Point v01 = {xRel*stepPerPixel, -(1-yRel*stepPerPixel)};
+            Point v11 = {-(1-xRel*stepPerPixel), -(1-yRel*stepPerPixel)};
+            
+            
+            
             int currCellX = x/pixelsPerCell;
             int currCellY = y/pixelsPerCell;
+            //cout << currCellX << " " << currCellY << endl;
             Point grad00 = gradients[currCellX][currCellY];
             Point grad10 = gradients[currCellX+1][currCellY];
             Point grad01 = gradients[currCellX][currCellY+1];
@@ -99,20 +121,28 @@ std::vector<std::vector<float>> generatePerlinNoise(int width, int height, int p
             
             
             
-            float interpolationX0 = dotProd(grad00, currPixel) + fadeFunc(currPixel.x) * (dotProd(grad10, currPixel) - dotProd(grad00, currPixel));
+            float interpolationX0 = dotProd(grad00, v00) + fadeFunc(v00.x) * (dotProd(grad10, v10) - dotProd(grad00, v00));
 
-            float interpolationX1 = dotProd(grad01, currPixel) + fadeFunc(currPixel.x) * (dotProd(grad11, currPixel) - dotProd(grad01, currPixel));
+            float interpolationX1 = dotProd(grad01, v01) + fadeFunc(v00.x) * (dotProd(grad11, v11) - dotProd(grad01, v01));
             //interpolation in Y
-            float finalVal = interpolationX0 + fadeFunc(currPixel.y) * (interpolationX1 - interpolationX0);
+            float finalVal = interpolationX0 + fadeFunc(v00.y) * (interpolationX1 - interpolationX0);
             
+//            cout << "pixel: " << x << " " << y << endl;
+//            cout << "cell: " << currCellX << " " << currCellY << endl;
+//            cout << "grad00: " << grad00.x << " " << grad00.y << endl;
+//            cout << "grad10: " << grad10.x << " " << grad10.y << endl;
+//            cout << "grad01: " << grad01.x << " " << grad01.y << endl;
+//            cout << "grad11: " << grad11.x << " " << grad11.y << endl << endl;
             
-            cout << "grad00: " << grad00.x << " " << grad00.y << endl;
-            cout << "Point: " << currPixel.x << " " << currPixel.y << endl;
-            cout << "Dot Prod: " << dotProd(grad00, currPixel) << endl;
+           // cout << "Point: " << currPixel.x << " " << currPixel.y << endl;
+            //cout << "Dot Prod: " << dotProd(grad00, currPixel) << endl;
+            
             
             
             
             noise[x][y] = finalVal;
+            
+            cout << finalVal << endl;
         }
     }
     
@@ -124,8 +154,8 @@ std::vector<std::vector<float>> generatePerlinNoise(int width, int height, int p
 int main(int argc, char* argv[]) {
 //    int width = 512;  // Width of the window and the noise map
 //    int height = 512; // Height of the window and the noise map
-    int width = 512;
-    int height = 512;
+    int width = 500;
+    int height = 500;
 
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -151,7 +181,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Generate Perlin noise values
-    std::vector<std::vector<float>> noise = generatePerlinNoise(width, height, 64);
+    std::vector<std::vector<float>> noise = generatePerlinNoise(width, height, 100);
 
     // Create a texture to render the noise
     SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, width, height);
@@ -167,7 +197,7 @@ int main(int argc, char* argv[]) {
     std::vector<Uint32> pixels(width * height);
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            float value = noise[x][y];
+            float value = (noise[x][y] + 1)/2;
             Uint8 color = static_cast<Uint8>(value * 255);
             pixels[y * width + x] = (255 << 24) | (color << 16) | (color << 8) | color; // ARGB format
         }
